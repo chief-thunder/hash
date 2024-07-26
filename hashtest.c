5ccb4a2c7d26c06237690f790da44c3302d3c7f4
ct <150102328+chief-thunder@users.noreply.github.com>
2024-07-26 22:36:28 +1000

    Used macOS object-c specific secure random function.

--- hashtest.c
+++ hashtest.c
@@ -14,7 +14,7 @@ char charset[] = "abcdefghijklmnopqrstuvwxyz"
                  "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 // Set password.
-char password[] = "Thisisreallyg00d";
+char password[] = "Thisisreallyg00d!";
 
 // Create salt function.
 /*
@@ -27,10 +27,24 @@ char getsalt(char saltstr[])
 int main(void)
 {
     int salt = 0;
+    int errSecSuccess = 0;
     getentropy(&salt, 1);
     printf("Result: %d", salt);
-    //printf("Contents of charset: %s", charset);
-    //printf("Contents of password: %s", password);
+
+    short int bytes[10];
+    int status = SecRandomCopyBytes(charset, (sizeof bytes)/(sizeof bytes[0]), &bytes);
+        
+    if (status == errSecSuccess) { // Always test the status.
+        for (int i = 0; i < (sizeof bytes)/(sizeof bytes[0]); i++) {
+            printf("%d", bytes[i]);
+        }
+        // Prints something different every time you run.
+    }
+
+    printf("This is the random: %s", secretkey);
+
+    printf("Contents of charset: %s", charset);
+    printf("Contents of password: %s", password);
     
     return 0;
 }
