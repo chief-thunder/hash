2e1a07886f0510d27ebb976ee7a74bbc84890285
ct <150102328+chief-thunder@users.noreply.github.com>
2024-07-27 14:47:39 +1000

    Added Swift code.

---
+++ hashtest.swift
@@ -0,0 +1,32 @@
+/*
+    Written 26/07/2024 by ct.
+    A program that salts and hashes a password.
+*/
+
+import Foundation
+
+var bytes = [Int8](repeating: 0, count: 10)
+let status = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
+
+
+if status == errSecSuccess { // Always test the status.
+    print(bytes)
+    // Prints something different every time you run.
+}
+
+/*
+    Start of logic.
+*/
+
+// Set a password.
+let password = "Thisisreallyg00d!"
+
+// Set the characters to use.
+let alphabet = "abcdefghijklmnopqrstuvwxyz" +
+                       "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                       "1234567890" +
+                       "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+// Print it.
+print("Password: \(password)")
+print("Alphabet: \(alphabet)")
