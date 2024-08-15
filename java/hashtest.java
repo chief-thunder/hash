6a932a5fef27a2b9b081d1d55fe1872b63d3553b
ct <150102328+chief-thunder@users.noreply.github.com>
2024-08-15 19:14:13 +1000

    Create hashtest.java

---
+++ java/hashtest.java
@@ -0,0 +1,12 @@
+/*
+ * Hashing and salting passwords in Python.
+ * Written by ct 4/08/2024.
+ */
+
+class hashtest
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
