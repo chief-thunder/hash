1ff3481fe1376c746db7e8c2eb35fb7d1352a744
ct <150102328+chief-thunder@users.noreply.github.com>
2024-09-21 22:51:53 +1000

    Create hashtest.rs
    Created folder and file in Rust.

---
+++ rust/hashtest.rs
@@ -0,0 +1,12 @@
+###
+# Hashing and salting passwords in Rust. 🦀
+# Written by ct 21/09/2024.
+###
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+}
