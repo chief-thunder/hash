d18ee2d08ed375dc3510440ba4693344e357252c
ct <150102328+chief-thunder@users.noreply.github.com>
2025-03-25 21:32:32 +1100

    Updated macos program

--- c/macos/hashtest-macos.c
+++ c/macos/hashtest-macos.c
@@ -5,7 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/random.h>
+//#include <sys/random.h>
+#include <Security/Security.h>
 
 // Create alphanumerical string with punctuation.
 char charset[] = "abcdefghijklmnopqrstuvwxyz"
@@ -16,20 +17,43 @@ char charset[] = "abcdefghijklmnopqrstuvwxyz"
 // Set password.
 char password[] = "Thisisreallyg00d!";
 
-// Create salt function.
-/*
-char getsalt(char saltstr[])
-{
-
+char choice(const char *charset, size_t length) {
+    uint8_t randomIndex;
+    int status = SecRandomCopyBytes(kSecRandomDefault, sizeof(randomIndex), &randomIndex);
+    if (status != errSecSuccess) {
+        fprintf(stderr, "Error generating random byte\n");
+        exit(EXIT_FAILURE);
+    }
+    return charset[randomIndex % length];
 }
-*/
+
+
+// Create salt function
+char getsalt(char saltstr[], size_t salt_length, const char *charset, size_t charset_length) {
+    for (size_t i = 0; i < salt_length; i++) {
+        saltstr[i] = choice(charset, charset_length);
+    }
+    saltstr[salt_length] = '\0'; // Null-terminate the salt string
+
+    // Return the value.
+    //return saltstr;
+}
+
+// Create salt function.
+//char getsalt(char saltstr[])
+//{
+
+//}
+
 
 int main(void)
 {
-    int salt = 0;
-    int errSecSuccess = 0;
-    getentropy(&salt, 1);
-    printf("Result: %d", salt);
+    char salt[11] = 0;
+    //int errSecSuccess = 0;
+    //getentropy(&salt, 1);
+    getsalt(salt, 10, charset, sizeof(charset) - 1);
+
+    printf("Result: %s\n", salt);
 /*
     short int bytes[10];
     int status = SecRandomCopyBytes(charset, (sizeof bytes)/(sizeof bytes[0]), &bytes);
