adb207d1ca29970b149ecb796e20b625fa6bb4b2
ct <150102328+chief-thunder@users.noreply.github.com>
2025-04-06 19:15:52 +1000

    Updates for macOS: Finally got random entropy!
    - Worked out the getentropy function.
    - Created the getsalt() function.

--- c/macos/hashtest-macos.c
+++ c/macos/hashtest-macos.c
@@ -1,58 +1,60 @@
 /*
-    Hashing and salting passwords in C.
-    Written by ct 30/06/2024.
+    Hashing and salting passwords in C on macOS Sequoia.
+    Written by ct 30/06/2024, with the help of ChatGPT.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/random.h>
 
-// Create alphanumerical string with punctuation.
+// Define the charset
 char charset[] = "abcdefghijklmnopqrstuvwxyz"
-                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                 "1234567890"
-                 "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"1234567890"
+"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-// Set password.
-char password[] = "Thisisreallyg00d!";
+// Function to get a random character from the charset
+char get_random_char(const char *charset, size_t charset_length) {
+    unsigned char random_byte;
 
-// Create salt function.
-/*
-char getsalt(char saltstr[])
-{
-
-}
-*/
-
-int main(void)
-{
-    int salt = 0;
-    int errSecSuccess = 0;
-    getentropy(&salt, 1);
-    printf("Result: %d", salt);
-
-    short int bytes[10];
-    int status = getentropy(bytes, sizeof(bytes));
-    int test = getentropy(charset, 10);
-        
-    if (status == 0) { // Always test the status.
-        for (int i = 0; i < (sizeof bytes)/(sizeof bytes[0]); i++) {
-            printf("%d ", bytes[i]);
-        }
-        printf("\n");
-        // Prints something different every time you run.
-    } else {
-        printf("Failed to generate random bytes\n");
+    // Use getentropy to generate a random byte
+    if (getentropy(&random_byte, sizeof(random_byte)) != 0) {
+        perror("getentropy failed");
+        exit(EXIT_FAILURE);
     }
 
-    printf("This is the random: %d\n", status);
-    printf("This is test: %d\n", test);
-    printf("Result: %d\n", salt);
+    // Use the random byte to select a character from the charset
+    return charset[random_byte % charset_length];
+}
 
-    int bob = 0;
-    printf("bob: %ld\n", sizeof(bob));
-    printf("Contents of charset: %s\n", charset);
-    printf("Contents of password: %s\n", password);
-    
-    return 0;
+char getsalt(char mystr[]) {
+
+    size_t charset_length = sizeof(charset) - 1; // Exclude the null terminator
+    unsigned int x;
+
+    for(x = 0; x < 10; x++) {
+        mystr[x] = get_random_char(charset, charset_length);
+    }
+
+    return mystr[10];
+}
+
+int main(void) {
+
+    size_t charset_length = sizeof(charset) - 1; // Exclude the null terminator
+
+    // Get a random character
+    char random_char = get_random_char(charset, charset_length);
+
+    // Print the random character
+    printf("Random character: %c\n", random_char);
+
+    // Generate a random salt
+    char salt[11]; // 10 characters + null terminator
+    getsalt(salt);
+    salt[10] = '\0'; // Null-terminate the string
+    printf("Random salt: %s\n", salt);
+
+
+    return EXIT_SUCCESS;
 }
